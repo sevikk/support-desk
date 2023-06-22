3dfa16a2c9a2d751989e32eed3727d12a5f88799
Sevikk <sevik.1989@gmail.com>
2023-06-22 15:29:45 -0500

    aa

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,14 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 const cors = require('cors')
 const app = express();
-app.use(cors())
+// app.use(cors())
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
